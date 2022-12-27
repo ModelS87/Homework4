b4aa149a8743ede041c05db92d9aa0af738fa695
volynkina <bzzz18@yandex.ru>
2022-12-27 19:56:22 +0500

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println(" Он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+        int age1 = 10;
+        if (age1 >= 18) {
+            System.out.println("Если возраст человека равен " + age1 + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age1 + ", он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
 }
