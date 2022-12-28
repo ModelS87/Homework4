a01166f3ef9182e76bbd3c6c55f438037420630e
volynkina <bzzz18@yandex.ru>
2022-12-28 19:47:27 +0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,12 @@ public class Main {
         } else {
             System.out.println("Если возраст человека равен " + age1 + ", он не достиг совершеннолетия, нужно немного подождать.");
         }
+    int temp = 4;
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градуса , нужно надеть шапку");
+        } else {
+            System.out.println("Сегодня" + temp + "градуса , можно идти без шапки");
+
+        }
     }
 }
